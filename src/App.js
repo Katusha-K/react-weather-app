224d7276b8c89b827e610f97506990f11c618513
Katusha-K <119417500+Katusha-K@users.noreply.github.com>
2024-01-04 20:26:21 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by Katia Kanievska{" "}
+          <a
+            href="https://github.com/Katusha-K/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            it is open-sourced.
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
