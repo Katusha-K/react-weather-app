6ab6a67fcc6edc991aec4df139951e6d8f51e381
Katusha-K <119417500+Katusha-K@users.noreply.github.com>
2024-01-16 14:22:13 +0100

    WeatherForecast.js has been corrected

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,7 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
-import Axios from "axios";
+import axios from "axios";
 
 export default function WeatherForecast(props) {
   function handleResponse(response) {}
@@ -9,9 +9,9 @@ export default function WeatherForecast(props) {
   let apiKey = "ae7531893d315790aca3946acd1731fc";
   let longtitude = props.coordinates.lon;
   let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longtitude}&appid=${apiKey}`;
-
-  https: Axios.get(apiUrl).then(handleResponse);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&appid=${apiKey}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(handleResponse);
 
   return (
     <div className="WeatherForecast">
