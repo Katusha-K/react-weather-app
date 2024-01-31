adb36f4eb77c8c46b097dedf091198b7ab4cc681
Katusha-K <119417500+Katusha-K@users.noreply.github.com>
2024-01-31 11:39:24 +0100

    Changed Default city name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Kyiv" />
         <footer>
           This project is coded by Katia Kanievska &{" "}
           <a
